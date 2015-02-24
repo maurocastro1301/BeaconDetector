8fbb9ab4092b358ac0579b42ea3e57430e921128
Paul Lamoreux <paul@fullsailstudios.com>
2015-02-24 11:48:43 -0800

    Experiment with new Lollipop (Android 5, Build 21) BLE libraries.

--- app/src/main/java/com/paullamoreux/apps/beacondetector/MainActivity.java
+++ app/src/main/java/com/paullamoreux/apps/beacondetector/MainActivity.java
@@ -1,5 +1,18 @@
 package com.paullamoreux.apps.beacondetector;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,9 +21,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.altbeacon.beacon.BeaconConsumer;
@@ -19,16 +30,24 @@ import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.Region;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity implements BeaconConsumer {
-    protected static final String TAG = "RangingActivity";
+    private static final String TAG = "BeaconDetectorMainActivity";
+    private final static int REQUEST_ENABLE_BT = 1;
 
+    private BluetoothManager bluetoothManager;
     private BeaconManager beaconManager;
     private ArrayList<String> messages;
     private ArrayAdapter<String> aMessages;
     private ListView lvMessages;
 
+    BluetoothAdapter bleAdapter;
+    BluetoothLeScanner scanner;
+    HashMap<String, BluetoothGatt> gatts = new HashMap<String, BluetoothGatt>();
+    HashMap<String, ScanResult> resultsByAddress = new HashMap<String, ScanResult>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +60,125 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         aMessages = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messages);
         lvMessages.setAdapter(aMessages);
 
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.bind(this);
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE Not Supported", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Initializes Bluetooth adapter.
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bleAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (bleAdapter == null || !bleAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            return;
+        }
+
+//        beaconManager = BeaconManager.getInstanceForApplication(this);
+//        beaconManager.bind(this);
+
+        //bleAdapter = BluetoothAdapter.getDefaultAdapter();
+        scanner = bleAdapter.getBluetoothLeScanner();
+        if (scanner != null) {
+            Toast.makeText(this, "scanner created", Toast.LENGTH_SHORT).show();
+            scanner.startScan(new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+
+
+                    ScanRecord record = result.getScanRecord();
+                    BluetoothDevice device = result.getDevice();
+                    int value = result.getRssi();
+
+                    String name = record.getDeviceName(); // device.getName();
+                    String address = device.getAddress();
+                    String message = name + ":" + address + ":" + String.valueOf(value);
+
+                    if (!resultsByAddress.containsKey(address)) {
+                        resultsByAddress.put(address, result);
+                        logToDisplay(message);
+                    }
+
+
+                    // If this is the first time we've seen this device, crank up a conversation
+                    // with it.
+
+//                    if (!gatts.containsKey(address)) {
+//                        BluetoothGatt gatt = device.connectGatt(MainActivity.this, true, new BluetoothGattCallback() {
+//                            @Override
+//                            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+//                                super.onConnectionStateChange(gatt, status, newState);
+//                                logToDisplay("onConnectionStateChange");
+//                            }
+//
+//                            @Override
+//                            public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+//                                super.onCharacteristicChanged(gatt, characteristic);
+//                                logToDisplay("onCharacteristicChanged");
+//                            }
+//
+//                            @Override
+//                            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+//                                super.onServicesDiscovered(gatt, status);
+//                                logToDisplay("onCharacteristicChanged");
+//                            }
+//                        });
+//                        gatts.put(address, gatt);
+//                        logToDisplay("Got a GATT for: " + address);
+//                    }
+
+                    //Toast.makeText(MainActivity.this, name + ":" + address + ":" + String.valueOf(value), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onBatchScanResults(List<ScanResult> results) {
+                    String message = "Got a batch scan result";
+                    logToDisplay(message);
+                }
+
+                @Override
+                public void onScanFailed(int errorCode) {
+                    String message = "unspecified failure";
+                    switch(errorCode) {
+                        case SCAN_FAILED_ALREADY_STARTED:
+                            message = "SCAN_FAILED_ALREADY_STARTED";
+                            break;
+
+                        case SCAN_FAILED_APPLICATION_REGISTRATION_FAILED:
+                            message = "SCAN_FAILED_APPLICATION_REGISTRATION_FAILED";
+                            break;
+
+                        case SCAN_FAILED_FEATURE_UNSUPPORTED:
+                            message = "SCAN_FAILED_FEATURE_UNSUPPORTED";
+                            break;
+
+                        case SCAN_FAILED_INTERNAL_ERROR:
+                            message = "SCAN_FAILED_INTERNAL_ERROR";
+                            break;
+                    }
+                    logToDisplay(message);
+                    //Toast.makeText(MainActivity.this, "onScanResult: " + message, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
+
+
+
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        beaconManager.unbind(this);
+        if (beaconManager != null) {
+            beaconManager.unbind(this);
+        }
+        if (scanner != null) {
+            //scanner.stopScan();
+        }
     }
 
 
@@ -100,6 +229,7 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
     private void logToDisplay(final String line) {
         runOnUiThread(new Runnable() {
             public void run() {
+                messages.add(line);
                 aMessages.notifyDataSetChanged();
             }
         });
